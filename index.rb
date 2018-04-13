25158792dcaa75f42fd7fa199af2ef74f06aed96
Marek <yurchylyak@gmail.com>
2018-04-13 12:05:12 +0300

    added index.rb

---
+++ index.rb
@@ -0,0 +1,79 @@
+require 'awesome_print'
+
+# ap 1 + 3
+# ap 2 + 2 * 2
+# ap 64**(1.0/2)
+# ap 3 * 3
+# ap 3/2
+# ap 3 + 1
+# ap 3 - 3
+# ap 3 > 2
+# ap 3 >= 3
+# ap 3 < 4
+# ap 3<= 3
+# ap 3 <=> 4
+# ap 1 == 1
+# ap 1 == 1.0
+# ap 'true' == 'true'
+# ap 1.eql?(1.0)
+# ap 1.eql?(1)
+# ap 'test' == 'test'
+# ap 'test' === 'test'
+# ap 'test'.equal?('test')
+# a = 1..100
+# ap a.include?(55)
+# ap a === 55
+# ap 5.class.superclass
+# ap 'test'.class
+# ap 5 && 3 > 2
+# a = nil
+# b = false
+# c = 5
+# ap !(a && b) && c
+# a = 5 || 4
+# ap a
+# b = 3
+# ap a > b ? 'Ruby' : 'Rails'
+# a = 10
+# if a == 10
+#   ap "a equals 10"
+# else
+#   ap "a doesn't equals 10"
+# end
+# x = rand(0..360)
+# if (0..90) === x
+#   ap "it's in the first quarter"
+# elsif (91..180) === x
+#   ap "it's in the second quarter"
+# elsif (181..270) === x
+#   ap "it's in the third quarter"
+# else
+#   ap "it's in the fourth quarter"
+# end
+# ap x
+# case
+# when (0..90) === x
+#   ap "it's in the first quarter"
+# when (91..180) === x
+#   ap "it's in the second quarter"
+# when (181..270) === x
+#   ap "it's in the third quarter"
+# else
+#   ap "it's in the fourth quarter"
+# end
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
